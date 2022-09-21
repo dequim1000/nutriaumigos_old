043b322d5a1e124374b7c3e732ca08baf72b3552
Lucas Jacomini <31415863+lucasjacomini31@users.noreply.github.com>
2022-09-20 23:59:39 -0300

    Add Fonts, create card and create slider

--- nutriaumigos/lib/Screens/Menu/home.page.dart
+++ nutriaumigos/lib/Screens/Menu/home.page.dart
@@ -6,6 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< Updated upstream
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Center(
@@ -50,9 +51,25 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+=======
+        backgroundColor: const Color.fromRGBO(3, 152, 158, 0.73),
+        body: Container(
+            height: 125,
+            margin: const EdgeInsets.symmetric(vertical: 60.0),
+            child: ListView(scrollDirection: Axis.horizontal, children: [
+              buildCard(context, 'Nutricionista',
+                  'assets/icons/patinhaColor_icon.png'),
+              const SizedBox(width: 12),
+              buildCard(context, 'Pet', 'assets/icons/patinha_icon.png'),
+              const SizedBox(width: 12),
+              buildCard(context, 'Alimentos', 'assets/icons/patinha_icon.png'),
+              const SizedBox(width: 12),
+              buildCard(context, 'FeedBack', 'assets/icons/patinha_icon.png'),
+            ])));
+>>>>>>> Stashed changes
   }
-}
 
+<<<<<<< Updated upstream
 Widget cardItem() {
   return Card(
     child: Column(
@@ -87,4 +104,43 @@ Widget cardItem() {
       ],
     ),
   );
+=======
+  Widget buildCard(BuildContext context, String name, String imageButton) =>
+      SizedBox(
+          width: 100,
+          height: 100,
+          child: Column(
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.pushReplacementNamed(context, 'menuPrincipal')
+                    },
+                    child: Container(
+                      width: 74,
+                      height: 74,
+                      decoration: BoxDecoration(
+                        color: const Color.fromRGBO(239, 239, 239, 1),
+                        border: Border.all(
+                            color: const Color.fromRGBO(239, 239, 239, 1),
+                            width: 3.0), // Set border width
+                        borderRadius: const BorderRadius.all(
+                            Radius.circular(10.0)), // Set rounded corner radius
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Image.asset(imageButton, height: 37, width: 37),
+                      ),
+                    ),
+                  )),
+              Text(name,
+                  style: const TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromRGBO(196, 196, 196, 1),
+                      fontSize: 16))
+            ],
+          ));
+>>>>>>> Stashed changes
 }
