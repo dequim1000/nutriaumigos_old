314d90d986ece04f671f04eb4c11d112c8ac6d44
André Otávio <dequim1000@gmail.com>
2022-10-18 13:37:36 -0300

    Criação de Conta

--- nutriaumigos/lib/Screens/Login/cadastro.dart
+++ nutriaumigos/lib/Screens/Login/cadastro.dart
@@ -1,9 +1,14 @@
+import 'dart:collection';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:nutriaumigos/constants.dart';
 import 'package:cpf_cnpj_validator/cpf_validator.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:nutriaumigos/methods/auth.dart';
 
 enum SingingCharacter { cliente, nutricionista }
 
@@ -15,22 +20,31 @@ class CadastroPage extends StatefulWidget {
 }
 
 class _CadastroPageState extends State<CadastroPage> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
   final _formKey = GlobalKey<FormState>();
   SingingCharacter? _character = SingingCharacter.cliente;
+
+  //Regex
   static final RegExp nameRegExp = RegExp('[a-zA-Z]');
   static final RegExp numberTelefoneExp = RegExp(r'(^(?:[+0]9)?[0-9]{10,12}$)');
-  static final RegExp emailRegExp = RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+  static final RegExp emailRegExp = RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+  //Mascaras
+  final maskCpf = MaskTextInputFormatter(
+      mask: "###.###.###-##", filter: {"#": RegExp(r'[0-9]')});
+  final maskNumber = MaskTextInputFormatter(
+      mask: "(##) #####-####", filter: {"#": RegExp(r'[0-9]')});
+  final maskData = MaskTextInputFormatter(mask: "##/##/####");
   @override
   Widget build(BuildContext context) {
-    var txtNome,
-        txtEmail,
-        txtSenha,
-        txtTelefone,
-        txtCpf,
-        txtDataNascimento,
-        txtIdade,
-        txtCodigoNutricionista = TextEditingController();
-
+    var txtNome = TextEditingController();
+    var txtEmail = TextEditingController(); 
+    var txtSenha = TextEditingController();
+    var txtTelefone = TextEditingController();
+    var txtCpf = TextEditingController();
+    var txtDataNascimento = TextEditingController();
+    var txtIdade = TextEditingController();
+    var txtCodigoNutricionista = TextEditingController();
     return Scaffold(
       body: Container(
           padding: const EdgeInsets.only(top: 10, left: 40, right: 40),
@@ -132,7 +146,6 @@ class _CadastroPageState extends State<CadastroPage> {
                 ),
                 TextFormField(
                   controller: txtNome,
-                  enabled: _character?.index == 0 ? true : false,
                   autofocus: true,
                   keyboardType: TextInputType.text,
                   decoration: const InputDecoration(
@@ -192,9 +205,9 @@ class _CadastroPageState extends State<CadastroPage> {
                   ),
                   style: const TextStyle(fontSize: 20),
                   validator: ((value) {
-                    if (value == null || value.isEmpty ) {
+                    if (value == null || value.isEmpty) {
                       return 'Preencha o campo de senha!';
-                    }else if(value.length < 8){
+                    } else if (value.length < 8) {
                       return 'Digite uma senha válida, maior que 8 caracteres!';
                     }
                     return null;
@@ -203,6 +216,7 @@ class _CadastroPageState extends State<CadastroPage> {
                 TextFormField(
                   controller: txtTelefone,
                   keyboardType: TextInputType.number,
+                  inputFormatters: [maskNumber],
                   decoration: const InputDecoration(
                     labelText: "Telefone",
                     labelStyle: TextStyle(
@@ -213,9 +227,10 @@ class _CadastroPageState extends State<CadastroPage> {
                   ),
                   style: const TextStyle(fontSize: 20),
                   validator: ((value) {
-                    if (value == null || value.isEmpty) {
+                    final phone = value?.replaceAll(RegExp(r'\D+'), '');
+                    if (phone == null || phone.isEmpty) {
                       return 'Preencha o campo de telefone!';
-                    }else if(!numberTelefoneExp.hasMatch(value)){
+                    } else if (phone.length != 11 || phone[2] != '9') {
                       return 'Digite um telefone válido!';
                     }
                     return null;
@@ -224,6 +239,7 @@ class _CadastroPageState extends State<CadastroPage> {
                 TextFormField(
                   controller: txtDataNascimento,
                   keyboardType: TextInputType.datetime,
+                  inputFormatters: [maskData],
                   decoration: const InputDecoration(
                     labelText: "Data de Nascimento",
                     labelStyle: TextStyle(
@@ -235,7 +251,7 @@ class _CadastroPageState extends State<CadastroPage> {
                   style: const TextStyle(fontSize: 20),
                   validator: ((value) {
                     if (value == null || value.isEmpty) {
-                      return 'Preencha o campo de CRMV';
+                      return 'Preencha o campo de data de nascimento';
                     }
                     return null;
                   }),
@@ -243,6 +259,7 @@ class _CadastroPageState extends State<CadastroPage> {
                 TextFormField(
                   controller: txtCpf,
                   keyboardType: TextInputType.number,
+                  inputFormatters: [maskCpf],
                   decoration: const InputDecoration(
                     labelText: "CPF",
                     labelStyle: TextStyle(
@@ -255,31 +272,32 @@ class _CadastroPageState extends State<CadastroPage> {
                   validator: ((value) {
                     if (value == null || value.isEmpty) {
                       return 'Preencha o campo de CPF';
-                    }else if(!CPFValidator.isValid(value)){
+                    } else if (!CPFValidator.isValid(value)) {
                       return 'Digite um CPF válido!';
                     }
                     return null;
                   }),
                 ),
-                TextFormField(
-                  controller: txtCodigoNutricionista,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: "CRMV",
-                    labelStyle: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
+                if (_character?.index == 1)
+                  TextFormField(
+                    controller: txtCodigoNutricionista,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: "CRMV",
+                      labelStyle: TextStyle(
+                        color: Colors.black38,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20,
+                      ),
                     ),
+                    style: const TextStyle(fontSize: 20),
+                    validator: ((value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Preencha o campo de CRMV';
+                      }
+                      return null;
+                    }),
                   ),
-                  style: const TextStyle(fontSize: 20),
-                  validator: ((value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Preencha o campo de CRMV';
-                    }
-                    return null;
-                  }),
-                ),
                 const SizedBox(
                   height: 10,
                 ),
@@ -311,9 +329,30 @@ class _CadastroPageState extends State<CadastroPage> {
                         ),
                         textAlign: TextAlign.center,
                       ),
-                      onPressed: () {
+                      onPressed: () async {
                         if (_formKey.currentState!.validate()) {
-                          criarConta(txtEmail.text, txtSenha.text, context);
+                          try {
+                            txtNome.text;
+                            await Authent().createUserwithEmailAndPassword(txtNome.text ,txtEmail.text, txtSenha.text, txtTelefone.text, txtDataNascimento.text, txtCpf.text, txtCodigoNutricionista.text ?? "", context).then((value){
+                              Navigator.pop(context);
+                            });
+                          } on FirebaseException catch (e) {
+                            var msg = '';
+                            if (e.code == 'email-already-in-use') {
+                              msg = 'ERRO: O email informado já está em uso';
+                            }else {
+                              msg = 'ERRO: ${e.message}';
+                            }
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(msg),
+                                duration: const Duration(
+                                  seconds: 2,
+                                ),
+                              ),
+                            );
+                          }
+                          //criarConta(txtNome.text ,txtEmail.text, txtSenha.text, txtTelefone.text, txtDataNascimento.text, txtCpf.text, txtCodigoNutricionista.text ?? "", context);
                         }
                       },
                     ),
@@ -339,34 +378,79 @@ class _CadastroPageState extends State<CadastroPage> {
     );
   }
 
-  void criarConta(email, senha, context) {
-    FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: email, password: senha)
-        .then((value) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Usuário criado com sucesso!'),
-          duration: Duration(
-            seconds: 2,
-          ),
-        ),
-      );
-      Navigator.pop(context);
-    }).catchError((erro) {
-      var msg = '';
-      if (erro.code == 'email-already-in-use') {
-        msg = 'ERRO: O email informado já está em uso';
-      } else {
-        msg = 'ERRO: ${erro.message}';
-      }
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(msg),
-          duration: const Duration(
-            seconds: 2,
-          ),
-        ),
-      );
-    });
+  void criarConta(String nome, String email, String senha, String telefone,
+      String data, String cpf, String crmv, context) async {
+    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email, password: senha);
+
+    Map<String, dynamic> userInfoMap = {
+      'nome': nome,
+      'email': email,
+      'telefone': telefone,
+      'data': data,
+      'cpf': cpf,
+      'crmv': crmv,
+    };
+
+    if (userCredential != null) {
+      DatabaseMethods().addUserInfoToDB(auth.currentUser!.uid, userInfoMap);
+    }
+
+    // FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: senha).then((value) {
+    //   salvarUsuario(nome ,email, senha, telefone, data, cpf, crmv, context);
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     const SnackBar(
+    //       content: Text('Usuário criado com sucesso!'),
+    //       duration: Duration(
+    //         seconds: 2,
+    //       ),
+    //     ),
+    //   );
+    //   Navigator.pop(context);
+    // }).catchError((erro) {
+    //   var msg = '';
+    //   if (erro.code == 'email-already-in-use') {
+    //     msg = 'ERRO: O email informado já está em uso';
+    //   } else {
+    //     msg = 'ERRO: ${erro.message}';
+    //   }
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     SnackBar(
+    //       content: Text(msg),
+    //       duration: const Duration(
+    //         seconds: 2,
+    //       ),
+    //     ),
+    //   );
+    // });
+  }
+
+  void salvarUsuario(nome, email, senha, telefone, data, cpf, crmv, context) {
+    String? usuarioID;
+    if (_character?.index == 0) {
+      FirebaseFirestore db = FirebaseFirestore.instance;
+      Map usuarios = new HashMap();
+      usuarios.putIfAbsent("nome", nome);
+      usuarios.putIfAbsent("telefone", telefone);
+
+      usuarioID = FirebaseAuth.instance.currentUser?.uid;
+
+      DocumentReference documentReference =
+          db.collection("Clientes").doc(usuarioID);
+      documentReference.set(usuarios);
+    } else {}
+  }
+}
+
+class DatabaseMethods {
+  Future addUserInfoToDB(String userId, Map<String, dynamic> userInfoMap) {
+    return FirebaseFirestore.instance
+        .collection("users")
+        .doc(userId)
+        .set(userInfoMap);
+  }
+
+  Future<DocumentSnapshot> getUserFromDB(String userId) async {
+    return FirebaseFirestore.instance.collection("users").doc(userId).get();
   }
 }
