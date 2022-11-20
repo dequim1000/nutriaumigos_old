f7c0412235e5747c50702d7191f2df82aa5c8ba1
André Otávio <dequim1000@gmail.com>
2022-11-20 20:49:07 -0300

    Teste

--- nutriaumigos/lib/Screens/Menu/home.page.dart
+++ nutriaumigos/lib/Screens/Menu/home.page.dart
@@ -133,62 +133,35 @@ class _HomePageState extends State<HomePage> {
       navegacaoTela = 'listaUsuarios';
     }
     return Container(
-      height: 120,
+      height: 100,
       margin: const EdgeInsets.only(top: 30, bottom: 10),
-      child: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(
-                      left: 7,
-                    ),
-                  ),
-                  _buildCard(context, nomeUsuario, tipoUsuario, iconeUsuario,
-                      'listaUsuarios', false, false),
-                  const SizedBox(width: 4),
-                  _buildCard(
-                      context,
-                      'Pet',
-                      tipoUsuario,
-                      'assets/icons/animais_icon.png',
-                      navegacaoTela,
-                      false,
-                      false),
-                  const SizedBox(width: 4),
-                  _buildCard(
-                      context,
-                      'Alimentos',
-                      tipoUsuario,
-                      'assets/icons/alimento_icon.png',
-                      navegacaoTela,
-                      true,
-                      false),
-                  const SizedBox(width: 4),
-                  _buildCard(
-                      context,
-                      'FeedBack',
-                      tipoUsuario,
-                      'assets/icons/feedback_icon.png',
-                      navegacaoTela,
-                      false,
-                      true),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                      right: 15,
-                    ),
-                  ),
-                ],
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 7,
               ),
             ),
-          ),
-        ],
-        // height: 120,
-        // margin: const EdgeInsets.only(top: 30, bottom: 10),
-        // child:
+            _buildCard(context, nomeUsuario, tipoUsuario, iconeUsuario,
+                'listaUsuarios', false, false),
+            const SizedBox(width: 4),
+            _buildCard(context, 'Pet', tipoUsuario,
+                'assets/icons/animais_icon.png', navegacaoTela, false, false),
+            const SizedBox(width: 4),
+            _buildCard(context, 'Alimentos', tipoUsuario,
+                'assets/icons/alimento_icon.png', navegacaoTela, true, false),
+            const SizedBox(width: 4),
+            _buildCard(context, 'FeedBack', tipoUsuario,
+                'assets/icons/feedback_icon.png', navegacaoTela, false, true),
+            const Padding(
+              padding: EdgeInsets.only(
+                right: 15,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
